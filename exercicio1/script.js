69038e16fe92e9c605e4a2d386a3587200d6ae45
luizfk <luiz.kessler7@gmail.com>
2022-08-30 11:39:14 -0300

    operadores

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+let par = Number(prompt("digite um número par"))
+let resp = par % 2
+console.log(`O restante do número é ${resp}`)
+
+//C- Sempre termina com 0.
+//D- Quando é digitado um número impar ele faz a conta normalmente.
